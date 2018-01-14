70d85886775b7b4fc4c9fa6bbffb412ef7621eac
Muhammad Usman <muhammad@weebly.com>
2018-01-14 12:23:23 -0800

    a little bit more progress

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
     <ul>
-      <li v-for="coin in coinData">
-        {{ coin.name }} (${{ coin.price_usd }})
+      <li v-for="coin in coinData" :class="cursor(coin)">
+        <div class="info">
+          <span class="rank">{{ coin.rank }}</span> 
+          <span class="name">{{ coin.name }}</span>
+        </div>
+        <div class="pricing">
+          <span class="price">
+            ${{ coin.price_usd }}
+          </span>
+          <span class="change">
+            {{ coin.percent_change_24h }}
+          </span>
+        </div>
       </li>
     </ul>
   </div>
@@ -16,7 +26,6 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Coins',
       coinData: {},
     }
   },
@@ -26,6 +35,16 @@ export default {
       axios.get('https://api.coinmarketcap.com/v1/ticker/?limit=10').then(function(res){
         self.coinData = res.data;
       });
+    },
+    cursor(coin) {
+      if (!coin) {
+        return 'ew-resize'
+      }
+      if (coin.percent_change_24h < 0) {
+        return 's-resize'
+      } else {
+        return 'n-resize'
+      }
     }
   },
   mounted() {
@@ -35,18 +54,62 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  width: 300px;
+  height: 420px;
+  // border: 1px solid;
+  font-size: 16px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  overflow-y: auto;
+  width: 100%;
+  height: 100%;
 }
 h1 {
   text-align: center;
 }
-body {
-  width: 300px;
-  height: 400px;
-  border: 1px solid;
+ul {
+  padding: 0;
+  margin: 0;
 }
+li {
+  list-style: none;
+  padding: 4px;
+  border-radius: 1px;
+  display: flex;
+  &:hover {
+    background-color: rgba(120, 120, 120, 0.3);
+  }
+}
+.info {
+  width: 50%;
+  display: flex;
+  align-items: center;
+  .rank {
+    padding-right: 10px;
+    font-size: 10px;
+  }
+}
+
+.pricing {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  text-align: right;
+  font-family: monaco, monospace;
+  .change {
+    font-size: 12px;
+  }
+  .price {
+    font-size: 13px;
+  }
+}
+
+.n-resize        { cursor: n-resize; }
+.s-resize        { cursor: s-resize; }
+.ew-resize       { cursor: ew-resize; }
 </style>
